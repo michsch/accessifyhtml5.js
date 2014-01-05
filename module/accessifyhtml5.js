a78558343b25cc43fbec19768968694dabe8b3ad
Michael Schulze <ms@michsch.de>
2014-01-05 18:52:35 +0100

    new compiled and minified AMD module version

--- module/accessifyhtml5.js
+++ module/accessifyhtml5.js
@@ -8,14 +8,14 @@
 */
 
 /**
- * accessifyhtml5.js - v2.0.1 - 2013-06-26
+ * accessifyhtml5.js - v2.0.2 - 2014-01-05
  * https://github.com/michsch/accessifyhtml5.js
  * original: https://github.com/yatil/accessifyhtml5.js
  * Copyright (c) 2013 Eric Eggert, Michael Schulze (AMD wrapper); Licensed MIT license
 */
 
 (function(root, factory, sr) {
-  "use strict";
+  'use strict';
   if (typeof exports === 'object') {
     module.exports = factory();
   } else if (typeof define === 'function' && define.amd) {
@@ -27,9 +27,9 @@
   }
   return true;
 })((typeof window === 'object' && window) || this, function() {
-  "use strict";
+  'use strict';
   return function(defaults, more_fixes) {
-    var ATTR_SECURE, Doc, ID_PREFIX, attr, by_match, el_label, elems, fix, fixes, i, key, mo, n_label, obj, value;
+    var ATTR_SECURE, Doc, ID_PREFIX, attr, by_match, el_label, elems, error, fix, fixes, i, key, mo, n_label, obj, result, value;
     fixes = {
       article: {
         role: 'article'
@@ -53,7 +53,13 @@
         'aria-required': 'true'
       }
     };
-    ATTR_SECURE = /aria-[a-z]+|role|tabindex|title|alt|data-[\w\-]+|lang|style|maxlength|placeholder|pattern|type/;
+    result = {
+      ok: [],
+      warn: [],
+      fail: []
+    };
+    error = result.fail;
+    ATTR_SECURE = new RegExp('aria-[a-z]+|role|tabindex|title|alt|data-[\\w-]+|lang|' + 'style|maxlength|placeholder|pattern|required|type|target|accesskey|longdesc');
     ID_PREFIX = 'acfy-id-';
     n_label = 0;
     Doc = document;
@@ -78,29 +84,86 @@
           };
         }
       }
-      for (mo in more_fixes) {
-        fixes[mo] = more_fixes[mo];
+      if (more_fixes && more_fixes._CONFIG_ && more_fixes._CONFIG_.ignore_defaults) {
+        fixes = more_fixes;
+      } else {
+        for (mo in more_fixes) {
+          fixes[mo] = more_fixes[mo];
+        }
       }
       for (fix in fixes) {
+        if (fix.match(/^_(CONFIG|[A-Z]+)_/)) {
+          continue;
+        }
         if (fixes.hasOwnProperty(fix)) {
-          elems = Doc.querySelectorAll(fix);
+          try {
+            elems = Doc.querySelectorAll(fix);
+          } catch (ex) {
+            error.push({
+              sel: fix,
+              attr: null,
+              val: null,
+              msg: 'Invalid syntax for `document.querySelectorAll` function',
+              ex: ex
+            });
+          }
           obj = fixes[fix];
+          if (!elems || elems.length < 1) {
+            result.warn.push({
+              sel: fix,
+              attr: null,
+              val: null,
+              msg: 'Not found'
+            });
+          }
           i = 0;
           while (i < elems.length) {
             for (key in obj) {
               if (obj.hasOwnProperty(key)) {
                 attr = key;
                 value = obj[key];
-                if (!attr.match(ATTR_SECURE)) {
+                if (attr.match(/_?note/)) {
                   continue;
                 }
-                if (!(typeof value).match(/string|number/)) {
+                if (!attr.match(ATTR_SECURE)) {
+                  error.push({
+                    sel: fix,
+                    attr: attr,
+                    val: null,
+                    msg: 'Attribute not allowed',
+                    re: ATTR_SECURE
+                  });
+                  continue;
+                }
+                if (!(typeof value).match(/string|number|boolean/)) {
+                  error.push({
+                    sel: fix,
+                    attr: attr,
+                    val: value,
+                    msg: 'Value-type not allowed'
+                  });
                   continue;
                 }
                 by_match = attr.match(/(describ|label)l?edby/);
                 if (by_match) {
-                  el_label = Doc.querySelector(value);
+                  try {
+                    el_label = Doc.querySelector(value);
+                  } catch (ex) {
+                    error.push({
+                      sel: fix,
+                      attr: attr,
+                      val: value,
+                      msg: "Invalid selector syntax (2) - see 'val'",
+                      ex: ex
+                    });
+                  }
                   if (!el_label) {
+                    error.push({
+                      sel: fix,
+                      attr: attr,
+                      val: value,
+                      msg: "Labelledby ref not found - see 'val'"
+                    });
                     continue;
                   }
                   if (!el_label.id) {
@@ -112,6 +175,19 @@
                 }
                 if (!elems[i].hasAttribute(attr)) {
                   elems[i].setAttribute(attr, value);
+                  result.ok.push({
+                    sel: fix,
+                    attr: attr,
+                    val: value,
+                    msg: "Added"
+                  });
+                } else {
+                  result.warn.push({
+                    sel: fix,
+                    attr: attr,
+                    val: value,
+                    msg: 'Already present, skipped'
+                  });
                 }
               }
             }
@@ -120,6 +196,7 @@
         }
       }
     }
-    return true;
+    result.input = fixes;
+    return result;
   };
 }, 'AccessifyHTML5');
